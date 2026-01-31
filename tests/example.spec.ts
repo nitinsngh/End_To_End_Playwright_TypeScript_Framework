9272603db0ff2342c1b950fdf70925e08cdb2e39
Nitin Singh <nitinsngh834@gmail.com>
2026-01-31 12:42:51 +0530

    Change in example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -6,13 +6,3 @@ test('@Smoke Verify homepage title', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Automation Exercise/);
 });
-
-test('@Smoke get started link', async ({ page }) => {
-  await page.goto('https://automationexercise.com/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
